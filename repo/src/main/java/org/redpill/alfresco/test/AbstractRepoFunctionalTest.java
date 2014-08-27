f9b03af283f378e32b385f9b20e00c386b96e4bc
Niklas Ekman <niklas.ekman@gmail.com>
2014-08-27 17:02:36 +0200

    Major additions to the abstract functional test
    Signed-off-by: Niklas Ekman <niklas.ekman@gmail.com>

--- repo/src/main/java/org/redpill/alfresco/test/AbstractRepoFunctionalTest.java
+++ repo/src/main/java/org/redpill/alfresco/test/AbstractRepoFunctionalTest.java
@@ -2,68 +2,355 @@ package org.redpill.alfresco.test;
 
 import static com.jayway.restassured.RestAssured.*;
 import static org.hamcrest.Matchers.*;
+import static org.junit.Assert.*;
 
 import java.io.InputStream;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.commons.lang.StringUtils;
+import org.apache.log4j.Logger;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import com.jayway.restassured.RestAssured;
 import com.jayway.restassured.http.ContentType;
 import com.jayway.restassured.response.Response;
+import com.jayway.restassured.specification.RequestSpecification;
 
 public abstract class AbstractRepoFunctionalTest {
 
+  private final static Logger LOG = Logger.getLogger(AbstractRepoFunctionalTest.class);
+
   public static final String BASE_URI = "http://localhost:8080/alfresco/service";
 
+  protected JSONObject getMetadata(String nodeRef) throws JSONException {
+    RestAssured.requestContentType(ContentType.JSON);
+    RestAssured.responseContentType(ContentType.JSON);
+
+    Response response = given().baseUri(BASE_URI).pathParam("nodeRef", nodeRef).expect().statusCode(200).when().get("/api/metadata?nodeRef={nodeRef}&shortQNames=true");
+
+    if (LOG.isDebugEnabled()) {
+      response.prettyPrint();
+    }
+
+    return new JSONObject(response.asString());
+  }
+
+  protected void updateDocument(String nodeRef, Map<String, String> properties) throws JSONException {
+    RestAssured.requestContentType(ContentType.JSON);
+    RestAssured.responseContentType(ContentType.JSON);
+
+    JSONObject json = new JSONObject();
+    json.put("properties", properties);
+
+    String id = StringUtils.replace(nodeRef, "workspace://SpacesStore/", "");
+
+    Response response = given()
+        .baseUri(BASE_URI)
+        .pathParam("store_type", "workspace")
+        .pathParam("store_id", "SpacesStore")
+        .pathParam("id", id)
+        .request().body(json.toString())
+        .expect().statusCode(200)
+        .when().post("/api/metadata/node/{store_type}/{store_id}/{id}");
+    
+    if (LOG.isDebugEnabled()) {
+      response.prettyPrint();
+    }
+  }
+
+  protected JSONObject checkoutDocument(String nodeRef) throws JSONException {
+    RestAssured.requestContentType(ContentType.JSON);
+    RestAssured.responseContentType(ContentType.JSON);
+
+    String storeType = "workspace";
+    String storeId = "SpacesStore";
+    String id = StringUtils.replace(nodeRef, "workspace://SpacesStore/", "");
+
+    Response response = given().baseUri(BASE_URI).pathParam("store_type", storeType).pathParam("store_id", storeId).pathParam("id", id).body("{}").expect().statusCode(200).when()
+        .post("/slingshot/doclib/action/checkout/node/{store_type}/{store_id}/{id}");
+
+    return new JSONObject(response.asString());
+  }
+
+  protected InputStream downloadDocument(String downloadUrl, String responseContentType) {
+    RestAssured.responseContentType(responseContentType);
+
+    Response response = given().baseUri(BASE_URI).expect().statusCode(200).when().get(downloadUrl);
+
+    return response.getBody().asInputStream();
+  }
+
+  protected JSONObject cancelCheckoutDocument(String nodeRef) throws JSONException {
+    RestAssured.requestContentType(ContentType.JSON);
+    RestAssured.responseContentType(ContentType.JSON);
+
+    String storeType = "workspace";
+    String storeId = "SpacesStore";
+    String id = StringUtils.replace(nodeRef, "workspace://SpacesStore/", "");
+
+    Response response = given().baseUri(BASE_URI).pathParam("store_type", storeType).pathParam("store_id", storeId).pathParam("id", id).body("{}").expect().statusCode(200).when()
+        .post("/slingshot/doclib/action/cancel-checkout/node/{store_type}/{store_id}/{id}");
+
+    return new JSONObject(response.asString());
+  }
+
   protected String uploadDocument(String filename, String site) {
+    return uploadDocument(filename, site, null);
+  }
+
+  protected String uploadDocument(String filename, String site, String contentType) {
     RestAssured.requestContentType("multipart/form-data");
     RestAssured.responseContentType(ContentType.JSON);
-  
+
     InputStream inputStream = Thread.currentThread().getContextClassLoader().getResourceAsStream(filename);
     
-    return given()
+    RequestSpecification request = given()
         .baseUri(BASE_URI)
         .multiPart("filedata", filename, inputStream)
         .formParam("filename", filename)
         .formParam("siteid", site)
-        .formParam("containerid", "documentLibrary")
+        .formParam("containerid", "documentLibrary");
+    
+    if (StringUtils.isNotBlank(contentType)) {
+      request.formParam("contenttype", contentType);
+    }
+
+    Response response = request
         .expect().statusCode(200)
-        .when().post("/api/upload").path("nodeRef");
+        .when().post("/api/upload");
+    
+    if (LOG.isDebugEnabled()) {
+      response.prettyPrint();
+    }
+    
+    return response.path("nodeRef");
   }
 
   protected void createSite(String shortName) {
     RestAssured.requestContentType(ContentType.JSON);
     RestAssured.responseContentType(ContentType.JSON);
-  
+
     String visibility = "PRIVATE";
     String title = "Demoplats";
     String description = "This is a description";
     String siteDashboard = "site-dashboard";
-  
+
     given()
         .baseUri(BASE_URI)
         .body(
             "{\"visibility\":\"" + visibility + "\",\"title\":\"" + title + "\",\"shortName\":\"" + shortName + "\",\"description\":\"" + description + "\",\"sitePreset\":\"" + siteDashboard + "\"}")
-        .expect()
-        .contentType(ContentType.JSON)
-        .and().statusCode(200)
-        .and().body("shortName", equalTo(shortName))
-        .when().post("/api/sites");
+        .expect().contentType(ContentType.JSON).and().statusCode(200).and().body("shortName", equalTo(shortName)).when().post("/api/sites");
+  }
+
+  protected JSONObject createUser(String username, String password, String firstname, String lastname, String email) throws JSONException {
+    return createUser(username, password, firstname, lastname, email, null, null);
+  }
+
+  protected JSONObject createUser(String username, String password, String firstname, String lastname, String email, Map<String, String> properties, List<String> groups) throws JSONException {
+    RestAssured.requestContentType(ContentType.JSON);
+    RestAssured.responseContentType(ContentType.JSON);
+
+    JSONObject json = new JSONObject();
+
+    json.put("userName", username);
+    json.put("password", password);
+    json.put("firstName", firstname);
+    json.put("lastName", lastname);
+    json.put("email", email);
+
+    if (properties != null) {
+      for (String property : properties.keySet()) {
+        json.put(property, properties.get(property));
+      }
+    }
+
+    if (groups != null) {
+      json.put("groups", new JSONArray(groups));
+    }
+
+    Response response = given().baseUri(BASE_URI).body(json.toString()).expect().contentType(ContentType.JSON).and().statusCode(200)
+    // .and().body("shortName", equalTo(shortName))
+        .when().post("/api/people");
+
+    return new JSONObject(response.body().asString());
+  }
+
+  protected void deleteUser(String username) {
+    RestAssured.requestContentType(ContentType.JSON);
+    RestAssured.responseContentType(ContentType.JSON);
+
+    given().baseUri(BASE_URI).pathParam("username", username).expect().statusCode(200).when().delete("/api/people/{username}");
+  }
+
+  protected void addSiteMembership(String site, String username, String role) throws JSONException {
+    JSONObject person = new JSONObject();
+    person.put("userName", username);
+
+    JSONObject json = new JSONObject();
+    json.put("person", person);
+    json.put("role", role);
+
+    given().baseUri(BASE_URI).pathParam("shortname", site).body(json.toString()).expect().contentType(ContentType.JSON).and().statusCode(200).when().post("/api/sites/{shortname}/memberships");
   }
 
   protected void deleteSite(String shortName) {
     RestAssured.requestContentType(ContentType.JSON);
     RestAssured.responseContentType(ContentType.JSON);
-  
-    given().baseUri(BASE_URI).expect().statusCode(200).when().delete("/api/sites/" + shortName);
+
+    Response response = given().baseUri(BASE_URI).expect().statusCode(200).when().delete("/api/sites/" + shortName);
+
+    response.prettyPrint();
   }
 
   protected String getDocumentLibraryNodeRef(String site) {
     RestAssured.requestContentType(ContentType.JSON);
     RestAssured.responseContentType(ContentType.JSON);
-  
+
     Response response = given().pathParameter("shortName", site).pathParam("container", "documentLibrary").baseUri(BASE_URI).expect().statusCode(200).when()
         .get("/slingshot/doclib/container/{shortName}/{container}");
-  
+
     return response.path("container.nodeRef");
   }
+  
+  protected JSONObject search(String term, String tag, String site, String container, String sort, String query, String repo) throws JSONException {
+    RequestSpecification request = given()
+        .baseUri(BASE_URI);
+    
+    String queryString = "";
+    
+    if (StringUtils.isNotBlank(term)) {
+      request.pathParam("term", term);
+      
+      queryString += "?term={term}";
+    }
 
+    if (StringUtils.isNotBlank(tag)) {
+      request.pathParam("tag", tag);
+      
+      if (StringUtils.isBlank(queryString)) {
+        queryString = "?";
+      } else {
+        queryString = queryString + "&";
+      }
+      
+      queryString = queryString + "tag={tag}";
+    }
+
+    if (StringUtils.isNotBlank(site)) {
+      request.pathParam("site", site);
+      
+      if (StringUtils.isBlank(queryString)) {
+        queryString = "?";
+      } else {
+        queryString = queryString + "&";
+      }
+      
+      queryString = queryString + "site={site}";
+    }
+
+    if (StringUtils.isNotBlank(container)) {
+      request.pathParam("container", container);
+      
+      if (StringUtils.isBlank(queryString)) {
+        queryString = "?";
+      } else {
+        queryString = queryString + "&";
+      }
+      
+      queryString = queryString + "container={container}";
+    }
+
+    if (StringUtils.isNotBlank(sort)) {
+      request.pathParam("sort", sort);
+      
+      if (StringUtils.isBlank(queryString)) {
+        queryString = "?";
+      } else {
+        queryString = queryString + "&";
+      }
+      
+      queryString = queryString + "sort={sort}";
+    }
+
+    if (StringUtils.isNotBlank(query)) {
+      request.pathParam("query", query);
+      
+      if (StringUtils.isBlank(queryString)) {
+        queryString = "?";
+      } else {
+        queryString = queryString + "&";
+      }
+      
+      queryString = queryString + "query={query}";
+    }
+
+    if (StringUtils.isNotBlank(repo)) {
+      request.pathParam("repo", repo);
+      
+      if (StringUtils.isBlank(queryString)) {
+        queryString = "?";
+      } else {
+        queryString = queryString + "&";
+      }
+      
+      queryString = queryString + "repo={repo}";
+    }
+    
+    System.out.println(queryString);
+
+    Response response = request
+        .expect().statusCode(200)
+        .when().get("/slingshot/search" + queryString);
+
+    if (LOG.isDebugEnabled()) {
+      response.prettyPrint();
+    }
+    
+    return new JSONObject(response.asString());
+  }
+
+  public void assertHasAspect(String nodeRef, String aspect) throws JSONException {
+    JSONObject metadata = getMetadata(nodeRef);
+
+    assertHasAspect(metadata, aspect);
+  }
+
+  public static void assertHasAspect(JSONObject metadata, String expectedAspect) throws JSONException {
+    JSONArray aspects = metadata.getJSONArray("aspects");
+
+    for (int x = 0; x < aspects.length(); x++) {
+      String aspect = aspects.getString(x);
+
+      if (aspect.equals(expectedAspect)) {
+        return;
+      }
+    }
+
+    fail("Aspect '" + expectedAspect + "' not found.");
+  }
+
+  public void assertDocumentExist(String nodeRef) throws JSONException {
+    JSONObject metadata = getMetadata(nodeRef);
+
+    assertDocumentExist(metadata);
+  }
+
+  public static void assertDocumentExist(JSONObject metadata) throws JSONException {
+    assertTrue(metadata.length() > 0);
+  }
+
+  public void assertDocumentNotExist(String nodeRef) throws JSONException {
+    JSONObject metadata = getMetadata(nodeRef);
+
+    assertDocumentNotExist(metadata);
+  }
+
+  public static void assertDocumentNotExist(JSONObject metadata) throws JSONException {
+    assertEquals(0, metadata.length());
+  }
+  
 }
+
