412d4a2bd4611423be3b7060d31762018055eb45
Niklas Ekman <niklas.ekman@redpill-linpro.com>
2014-11-06 10:00:10 +0100

    Added changing of owner on uploaded documents

--- repo/src/main/java/org/redpill/alfresco/test/AbstractRepoIntegrationTest.java
+++ repo/src/main/java/org/redpill/alfresco/test/AbstractRepoIntegrationTest.java
@@ -4,14 +4,19 @@ import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.fail;
 
 import java.io.InputStream;
+import java.io.Serializable;
 import java.util.List;
+import java.util.Map;
 import java.util.Properties;
+
+import javax.annotation.Resource;
 
 import org.alfresco.model.ContentModel;
 import org.alfresco.repo.model.Repository;
 import org.alfresco.repo.policy.BehaviourFilter;
 import org.alfresco.repo.security.authentication.AuthenticationComponent;
 import org.alfresco.repo.security.authentication.AuthenticationUtil;
+import org.alfresco.repo.security.authentication.AuthenticationUtil.RunAsWork;
 import org.alfresco.repo.site.SiteModel;
 import org.alfresco.repo.transaction.RetryingTransactionHelper;
 import org.alfresco.repo.transaction.RetryingTransactionHelper.RetryingTransactionCallback;
@@ -22,6 +27,7 @@ import org.alfresco.service.cmr.repository.ContentWriter;
 import org.alfresco.service.cmr.repository.NodeRef;
 import org.alfresco.service.cmr.repository.NodeService;
 import org.alfresco.service.cmr.security.MutableAuthenticationService;
+import org.alfresco.service.cmr.security.OwnableService;
 import org.alfresco.service.cmr.security.PersonService;
 import org.alfresco.service.cmr.site.SiteInfo;
 import org.alfresco.service.cmr.site.SiteService;
@@ -44,7 +50,7 @@ import org.springframework.test.context.ContextConfiguration;
 public abstract class AbstractRepoIntegrationTest implements InstanceTestClassListener {
 
   private final static String NAMESPACE_BEGIN = "" + QName.NAMESPACE_BEGIN;
-  
+
   private ThreadLocal<Boolean> _requiresNew = new ThreadLocal<Boolean>();
 
   @Autowired
@@ -101,10 +107,13 @@ public abstract class AbstractRepoIntegrationTest implements InstanceTestClassLi
   @Qualifier("global-properties")
   protected Properties _properties;
 
+  @Resource(name = "OwnableService")
+  private OwnableService _ownableService;
+
   @Override
   public void beforeClassSetup() {
     _requiresNew.set(true);
-    
+
     _transactionHelper = _transactionService.getRetryingTransactionHelper();
   }
 
@@ -130,26 +139,58 @@ public abstract class AbstractRepoIntegrationTest implements InstanceTestClassLi
    * @param callback
    * @return
    */
-  protected NodeRef createUser(String userId, CreateUserCallback callback) {
-    if (!_authenticationService.authenticationExists(userId)) {
-      _authenticationService.createAuthentication(userId, "password".toCharArray());
-      PropertyMap properties = new PropertyMap(3);
-      properties.put(ContentModel.PROP_USERNAME, userId);
-      properties.put(ContentModel.PROP_FIRSTNAME, userId);
-      properties.put(ContentModel.PROP_LASTNAME, "Test");
-      properties.put(ContentModel.PROP_EMAIL, _properties.getProperty("mail.to.default"));
+  protected NodeRef createUser(final String userId, final CreateUserCallback callback) {
+    return AuthenticationUtil.runAsSystem(new RunAsWork<NodeRef>() {
 
-      NodeRef user = _personService.createPerson(properties);
+      @Override
+      public NodeRef doWork() throws Exception {
+        if (!_authenticationService.authenticationExists(userId)) {
+          _authenticationService.createAuthentication(userId, "password".toCharArray());
+          PropertyMap properties = new PropertyMap(3);
+          properties.put(ContentModel.PROP_USERNAME, userId);
+          properties.put(ContentModel.PROP_FIRSTNAME, userId);
+          properties.put(ContentModel.PROP_LASTNAME, "Test");
+          properties.put(ContentModel.PROP_EMAIL, _properties.getProperty("mail.to.default"));
 
-      if (callback != null) {
-        callback.onCreateUser(user);
+          NodeRef user = _personService.createPerson(properties);
+
+          if (callback != null) {
+            callback.onCreateUser(user);
+          }
+
+          return user;
+        } else {
+          fail("User exists: " + userId);
+          return null;
+        }
       }
 
-      return user;
-    } else {
-      fail("User exists: " + userId);
-      return null;
-    }
+    });
+  }
+
+  protected void deleteUser(final NodeRef personRef) {
+    AuthenticationUtil.runAsSystem(new RunAsWork<Void>() {
+
+      @Override
+      public Void doWork() throws Exception {
+        _personService.deletePerson(personRef);
+
+        return null;
+      }
+
+    });
+  }
+
+  protected void deleteUser(final String userName) {
+    AuthenticationUtil.runAsSystem(new RunAsWork<Void>() {
+
+      @Override
+      public Void doWork() throws Exception {
+        _personService.deletePerson(userName);
+
+        return null;
+      }
+    });
   }
 
   /**
@@ -260,15 +301,21 @@ public abstract class AbstractRepoIntegrationTest implements InstanceTestClassLi
     return uploadDocument(site, filename, inputStream, folders, name, parentNodeRef, null);
   }
 
+  protected FileInfo uploadDocument(SiteInfo site, String filename, InputStream inputStream, List<String> folders, String name, NodeRef parentNodeRef, String type) {
+    return uploadDocument(site, filename, inputStream, folders, name, parentNodeRef, null, null);
+  }
+
   protected FileInfo uploadDocument(final SiteInfo site, final String filename, final InputStream inputStream, final List<String> folders, final String name, final NodeRef parentNodeRef,
-      final String type) {
+      final String type, final Map<QName, Serializable> properties) {
     return _transactionHelper.doInTransaction(new RetryingTransactionCallback<FileInfo>() {
 
       @Override
       public FileInfo execute() throws Throwable {
         String finalName = StringUtils.isNotEmpty(name) ? name : FilenameUtils.getName(filename);
 
-        NodeRef documentLibrary = _siteService.getContainer(site.getShortName(), SiteService.DOCUMENT_LIBRARY);
+        // gets the folder to create the document in, if it's a site then it's
+        // the documentLibrary, if not then it's the user home
+        NodeRef documentLibrary = site != null ? _siteService.getContainer(site.getShortName(), SiteService.DOCUMENT_LIBRARY) : _repository.getUserHome(_repository.getPerson());
 
         NodeRef finalParentNodeRef = parentNodeRef != null ? parentNodeRef : documentLibrary;
 
@@ -279,9 +326,27 @@ public abstract class AbstractRepoIntegrationTest implements InstanceTestClassLi
           }
         }
 
-        FileInfo fileInfo = _fileFolderService.create(finalParentNodeRef, finalName, type == null ? ContentModel.TYPE_CONTENT : createQName(type));
+        QName nodeTypeQName = type == null ? ContentModel.TYPE_CONTENT : createQName(type);
+        
+        QName assocQName = QName.createQName(NamespaceService.CONTENT_MODEL_1_0_URI, QName.createValidLocalName(finalName));
+        
+        // creates the document
+        final NodeRef document = _nodeService.createNode(finalParentNodeRef, ContentModel.ASSOC_CONTAINS, assocQName, nodeTypeQName, properties).getChildRef();
 
-        ContentWriter writer = _contentService.getWriter(fileInfo.getNodeRef(), ContentModel.PROP_CONTENT, true);
+        // set the correct owner on the document
+        AuthenticationUtil.runAsSystem(new RunAsWork<Void>() {
+
+          @Override
+          public Void doWork() throws Exception {
+            _ownableService.setOwner(document, AuthenticationUtil.getFullyAuthenticatedUser());
+            return null;
+          }
+
+        });
+
+        FileInfo fileInfo = _fileFolderService.getFileInfo(document);
+
+        ContentWriter writer = _contentService.getWriter(document, ContentModel.PROP_CONTENT, true);
 
         writer.guessEncoding();
 
@@ -323,13 +388,13 @@ public abstract class AbstractRepoIntegrationTest implements InstanceTestClassLi
       deleteSite(site);
     }
   }
-  
+
   public interface RunInSite<Result> {
-    
+
     Result doInSite(SiteInfo site) throws Exception;
-    
+
   }
-  
+
   public void setRequiresNew(boolean requiresNew) {
     _requiresNew.set(requiresNew);
   }
@@ -337,7 +402,6 @@ public abstract class AbstractRepoIntegrationTest implements InstanceTestClassLi
   public boolean isRequiresNew() {
     return _requiresNew.get();
   }
-
 
   public interface CreateUserCallback {
 
@@ -356,4 +420,25 @@ public abstract class AbstractRepoIntegrationTest implements InstanceTestClassLi
     void onCreateSite(SiteInfo site);
 
   }
+
+  public void assertType(String message, NodeRef node, QName type) {
+    if (!_nodeService.getType(node).isMatch(type)) {
+      fail(message);
+    }
+  }
+
+  public void assertType(NodeRef node, QName type) {
+    assertType(null, node, type);
+  }
+
+  public void assertHasAspect(String message, NodeRef node, QName aspect) {
+    if (!_nodeService.hasAspect(node, aspect)) {
+      fail(message);
+    }
+  }
+
+  public void assertHasAspect(NodeRef node, QName aspect) {
+    assertHasAspect(null, node, aspect);
+  }
+
 }
