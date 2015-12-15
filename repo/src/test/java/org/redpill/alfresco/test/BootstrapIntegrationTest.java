69f28d121c1dc04c7233fc8726105ceefd661bbe
Marcus Svartmark <marcus.svartmark@redpill-linpro.com>
2015-12-15 13:40:34 +0100

    Add integration test

---
+++ repo/src/test/java/org/redpill/alfresco/test/BootstrapIntegrationTest.java
@@ -0,0 +1,11 @@
+package org.redpill.alfresco.test;
+
+import org.junit.Test;
+
+public class BootstrapIntegrationTest extends AbstractRepoIntegrationTest {
+
+  @Test
+  public void testNothing() {
+    
+  }
+}
