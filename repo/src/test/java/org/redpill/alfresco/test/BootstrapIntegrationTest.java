9b292429dc555fd204d509d3abd176f7cea8bc13
Marcus Svartmark <marcus.svartmark@redpill-linpro.com>
2016-05-11 11:26:52 +0200

    Updated for Alfresco 5.1

--- repo/src/test/java/org/redpill/alfresco/test/BootstrapIntegrationTest.java
+++ repo/src/test/java/org/redpill/alfresco/test/BootstrapIntegrationTest.java
@@ -8,22 +8,22 @@ public class BootstrapIntegrationTest extends AbstractRepoIntegrationTest {
 
   @Test
   public void testComponentsAreWired() {
-   assertNotNull(_authenticationComponent);
-   assertNotNull(_transactionService);
-   assertNotNull(_transactionHelper);
-   assertNotNull(_behaviourFilter);
-   assertNotNull(_repository);
-   assertNotNull(_siteService);
-   assertNotNull(_authenticationService);
-   assertNotNull(_personService);
-   assertNotNull(_nodeService);
-   assertNotNull(_fileFolderService);
-   assertNotNull(_namespaceService);
-   assertNotNull(_contentService);
-   assertNotNull(_workflowService);
-   assertNotNull(_authorityService);
-   assertNotNull(_permissionService);
-   assertNotNull(_properties);
-   assertNotNull(_ownableService);
+    assertNotNull(_authenticationComponent);
+    assertNotNull(_transactionService);
+    assertNotNull(_transactionHelper);
+    assertNotNull(_behaviourFilter);
+    assertNotNull(_repository);
+    assertNotNull(_siteService);
+    assertNotNull(_authenticationService);
+    assertNotNull(_personService);
+    assertNotNull(_nodeService);
+    assertNotNull(_fileFolderService);
+    assertNotNull(_namespaceService);
+    assertNotNull(_contentService);
+    assertNotNull(_workflowService);
+    assertNotNull(_authorityService);
+    assertNotNull(_permissionService);
+    assertNotNull(_properties);
+    assertNotNull(_ownableService);
   }
 }
